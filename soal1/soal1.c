c3ea5bd9da2b0930fb005ccee41380dc40318e7c
TropEX <61292802+TropEX@users.noreply.github.com>
2020-03-13 16:41:33 +0700

    Update soal1.c

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -11,7 +11,7 @@
 #include <time.h>
 #include <sys/wait.h>
 
-int isApa(char isi[]){
+int angkacheck(char isi[]){
    if(strlen(isi) == 1)
         if(isi[0] == '*')
             return 1;
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]) {
 //cek detik sesuai argumen yang diinputkan
 
   if(strcmp(argv[1],"*") == 0) detik = 60;
-  else if(isApa(argv[1])){
+  else if(angkacheck(argv[1])){
     detik = atoi(argv[1]);
   }
    else  if(detik > 59 || detik < 0){
@@ -52,7 +52,7 @@ int main(int argc, char *argv[]) {
   //cek apakah menit sesuai argumen yang diinputkan
 
   if(strcmp(argv[2],"*") == 0) menit = 60;
-  else if(isApa(argv[2])){
+  else if(angkacheck(argv[2])){
     menit = atoi(argv[2]);
   }
   else  if(menit > 59 || menit < 0){
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
   // Mengecek apakah jam sesuai argumen yang diinginkan
 
   if(strcmp(argv[3],"*") == 0) jam = 24;
-  else if(isApa(argv[3])){
+  else if(angkacheck(argv[3])){
     jam = atoi(argv[3]);
   }
   else  if(jam > 23 || jam < 0){
@@ -81,8 +81,6 @@ int main(int argc, char *argv[]) {
       return 0;
     }
 
-
-
   pid_t pid, sid;
 
   pid = fork();
@@ -90,7 +88,6 @@ int main(int argc, char *argv[]) {
   if (pid < 0) {
     exit(EXIT_FAILURE);
   }
-
 
   if (pid > 0) {
     exit(EXIT_SUCCESS);
@@ -112,13 +109,12 @@ int main(int argc, char *argv[]) {
   close(STDERR_FILENO);
 
 while (1) {
-
      time_t timer = time(NULL);
      struct tm *tm_info = localtime(&timer);
 
      detik_tm = tm_info->tm_sec;
      menit_tm = tm_info->tm_min;
-       jam_tm = tm_info->tm_hour;
+     jam_tm = tm_info->tm_hour;
 
       if ((tm_info->tm_sec == detik || detik == 60) && (tm_info->tm_min == menit || menit == 60)
         && (tm_info->tm_hour == jam || jam == 24))
